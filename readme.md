b0cb50687ef850b805e7ae1edd6c10bb4a8c0cfe
Christopher Bischoff <hallo@overcrowded.de>
2018-03-04 15:29:42 +0100

    Initial commit
    Signed-off-by: Christopher Bischoff <hallo@overcrowded.de>

---
+++ readme.md
@@ -0,0 +1,15 @@
+> Please note: The aproach I've used is _very_ hacky 🙈 Please let me know, if you have a better solution.
+
+# 📦 parcel-plugin-html
+
+Adds `data-src` to the list of all attributes that may produce a dependency. Supported elements are `<script>`, `<img>`, `<audio>`, `<video>`, `<source>`, `<track>`, `<iframe>` and `<embed>`.
+
+## Install
+
+Make sure you have [installed parcel](https://parceljs.org/getting_started.html) before continuing.
+
+```bash
+yarn add --dev parcel-plugin-foo
+# or
+npm install --save-dev parcel-plugin-foo
+```    
---
+++ src/assets/AnotherHTMLAsset.js
@@ -0,0 +1,104 @@
+const HTMLAsset = require('../../node_modules/parcel-bundler/src/assets/HTMLAsset');
+
+// A list of all attributes that may produce a dependency
+// Based on https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
+const ATTRS = {
+  src: [
+    'script',
+    'img',
+    'audio',
+    'video',
+    'source',
+    'track',
+    'iframe',
+    'embed'
+  ],
+  'data-src': [
+    'script',
+    'img',
+    'audio',
+    'video',
+    'source',
+    'track',
+    'iframe',
+    'embed'
+  ],
+  href: ['link', 'a'],
+  poster: ['video'],
+  'xlink:href': ['use'],
+  content: ['meta']
+};
+
+// A list of metadata that should produce a dependency
+// Based on:
+// - http://schema.org/
+// - http://ogp.me
+// - https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup
+// - https://msdn.microsoft.com/en-us/library/dn255024.aspx
+const META = {
+  property: [
+    'og:image',
+    'og:image:url',
+    'og:image:secure_url',
+    'og:audio',
+    'og:audio:secure_url',
+    'og:video',
+    'og:video:secure_url'
+  ],
+  name: [
+    'twitter:image',
+    'msapplication-square150x150logo',
+    'msapplication-square310x310logo',
+    'msapplication-square70x70logo',
+    'msapplication-wide310x150logo',
+    'msapplication-TileImage'
+  ],
+  itemprop: [
+    'image',
+    'logo',
+    'screenshot',
+    'thumbnailUrl',
+    'contentUrl',
+    'downloadUrl'
+  ]
+};
+
+class AnotherHTMLAsset extends HTMLAsset {
+  collectDependencies() {
+    this.ast.walk(node => {
+      if (node.attrs) {
+        if (node.tag === 'meta') {
+          if (
+            !Object.keys(node.attrs).some(attr => {
+              let values = META[attr];
+              return values && values.includes(node.attrs[attr]);
+            })
+          ) {
+            return node;
+          }
+        }
+
+        for (let attr in node.attrs) {
+          if (node.tag === 'img' && attr === 'srcset') {
+            node.attrs[attr] = this.collectSrcSetDependencies(node.attrs[attr]);
+            this.isAstDirty = true;
+            continue;
+          }
+          let elements = ATTRS[attr];
+          // Check for virtual paths
+          if (node.tag === 'a' && node.attrs[attr].lastIndexOf('.') < 1) {
+            continue;
+          }
+          if (elements && elements.includes(node.tag)) {
+            node.attrs[attr] = this.processSingleDependency(node.attrs[attr]);
+            this.isAstDirty = true;
+          }
+        }
+      }
+
+      return node;
+    });
+  }
+}
+
+module.exports = AnotherHTMLAsset;
